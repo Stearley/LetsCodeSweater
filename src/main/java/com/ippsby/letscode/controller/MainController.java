061d6f1f2a2d35c8cb1fc3fe757f9a0b569971b9
stearley <woppwopp@mail.ru>
2020-11-18 18:23:16 +0300

    123

--- src/main/java/com/ippsby/letscode/controller/MainController.java
+++ src/main/java/com/ippsby/letscode/controller/MainController.java
@@ -1,8 +1,9 @@
-package com.ippsby.letscode;
+package com.ippsby.letscode.controller;
 
 import com.ippsby.letscode.domain.Message;
+import com.ippsby.letscode.domain.User;
 import com.ippsby.letscode.repository.MessageRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,10 +12,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Map;
 
 @Controller
-public class GreetingController {
+public class MainController {
 
-    @Autowired
-    private MessageRepository messageRepository;
+    private final MessageRepository messageRepository;
+
+    public MainController(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
 
     @GetMapping("/")
     public String greeting (Map<String , Object> model){
@@ -26,12 +30,16 @@ public class GreetingController {
         Iterable<Message> messages = messageRepository.findAll();
 
         model.put("messages" , messages);
+
         return "main";
     }
 
     @PostMapping("/main")
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model){
-        Message message = new Message(text, tag);
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag, Map<String, Object> model){
+        Message message = new Message(text, tag, user);
 
         messageRepository.save(message);
 
@@ -54,6 +62,7 @@ public class GreetingController {
 
         return "main";
     }
+
 
 }
 
--- src/main/java/com/ippsby/letscode/domain/Message.java
+++ src/main/java/com/ippsby/letscode/domain/Message.java
@@ -1,9 +1,6 @@
 package com.ippsby.letscode.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 
 @Entity
@@ -16,13 +13,21 @@ public class Message {
 
     private String tag;
 
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
+
     public Message() {
 
     }
 
-    public Message(String text, String tag) {
+    public Message(String text, String tag, User user) {
         this.text = text;
+        this.author = user;
         this.tag = tag;
+    }
+    public String getAuthorName(){
+        return author != null ? author.getUsername(): "<none>";
     }
 
     public int getId() {
@@ -47,6 +52,14 @@ public class Message {
 
     public void setTag(String tag) {
         this.tag = tag;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
     }
 }
 
