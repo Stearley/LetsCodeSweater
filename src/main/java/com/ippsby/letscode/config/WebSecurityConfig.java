c0cd4e13560e0fc0da68f2b099e41e8211e2c5ca
stearley <woppwopp@mail.ru>
2020-11-20 16:42:17 +0300

    123

--- src/main/java/com/ippsby/letscode/config/WebSecurityConfig.java
+++ src/main/java/com/ippsby/letscode/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.ippsby.letscode.config;
 import com.ippsby.letscode.service.UserService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,6 +12,7 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserService userService;
@@ -24,7 +26,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/registration").permitAll()
+                    .antMatchers("/", "/registration", "/static/**").permitAll()
                     .anyRequest().authenticated()
                 .and()
                     .formLogin()
--- src/main/java/com/ippsby/letscode/controller/MainController.java
+++ src/main/java/com/ippsby/letscode/controller/MainController.java
@@ -3,13 +3,19 @@ package com.ippsby.letscode.controller;
 import com.ippsby.letscode.domain.Message;
 import com.ippsby.letscode.domain.User;
 import com.ippsby.letscode.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
+import java.util.UUID;
 
 @Controller
 public class MainController {
@@ -19,6 +25,8 @@ public class MainController {
     public MainController(MessageRepository messageRepository) {
         this.messageRepository = messageRepository;
     }
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @GetMapping("/")
     public String greeting (Map<String , Object> model){
@@ -26,10 +34,16 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main (Map<String , Object> model){
+    public String main (@RequestParam(required = false, defaultValue = "") String filter, Model model){
         Iterable<Message> messages = messageRepository.findAll();
 
-        model.put("messages" , messages);
+        if (filter !=null && !filter.isEmpty()) {
+            messages = messageRepository.findByTag(filter);
+        } else {
+            messages = messageRepository.findAll();
+        }
+        model.addAttribute("messages" , messages);
+        model.addAttribute("filter", filter);
 
         return "main";
     }
@@ -38,8 +52,24 @@ public class MainController {
     public String add(
             @AuthenticationPrincipal User user,
             @RequestParam String text,
-            @RequestParam String tag, Map<String, Object> model){
+            @RequestParam String tag, Map<String, Object> model,
+            @RequestParam("file") MultipartFile file
+
+    ) throws IOException {
         Message message = new Message(text, tag, user);
+        if (file != null && !file.getOriginalFilename().isEmpty()){
+            File uploadDir = new File(uploadPath);
+            if (!uploadDir.exists()){
+                uploadDir.mkdir();
+            }
+            String uuidFile = UUID.randomUUID().toString();
+            System.out.println(file.getOriginalFilename());
+            String resultFilename = uuidFile + "." + file.getOriginalFilename().split("\\.")[1];
+
+            file.transferTo(new File( resultFilename));
+
+            message.setFilename(resultFilename);
+        }
 
         messageRepository.save(message);
 
@@ -50,18 +80,6 @@ public class MainController {
         return "main";
     }
 
-    @PostMapping("/filter")
-    public String filter(@RequestParam String filter,  Map<String, Object> model){
-        Iterable <Message> messages;
-        if (filter !=null && !filter.isEmpty()) {
-             messages = messageRepository.findByTag(filter);
-        } else {
-            messages = messageRepository.findAll();
-        }
-        model.put("messages", messages);
-
-        return "main";
-    }
 
 
 }
